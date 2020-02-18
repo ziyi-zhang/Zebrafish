d6966ac45643f2ff97323ed055331b74c12b7340
Ziyi - Desktop <shangyuyu@gmail.com>
2020-02-17 22:06:50 -0500

    Sync between computers

--- Matlab Scripts/imshowTiff.m
+++ Matlab Scripts/imshowTiff.m
@@ -1,5 +1,5 @@
-function [] = ImshowTiff(image, index, enhanced)
-% [] = ImshowTiff(image[, index][, enhanced])
+function [] = ImshowTiff(image, index, enhanced, zoomIn)
+% [] = ImshowTiff(image[, index][, enhanced][, zoomIn])
 % [] = ImshowTiff(mat)
 % 'image' should be a struct with meta data created by 'readMultipageTiff.m'
 % 'image.image' is a 3D matrix of size [length*width*stacks] eg:
@@ -7,6 +7,7 @@ function [] = ImshowTiff(image, index, enhanced)
 % 'index' is an array containing the desired indexes of subimages
 % 'enhanced' is a flag indicating whether the output should be shown with 
 % enhanced contrast
+% 'zoomIn' is the ratio of zoom in and appies on all subplots
 % 'mat' is a 2D/3D matrix. The contrast will be auto enhanced. If the input
 % is 3D, the third dimension will be summed
 % Ziyi. Feb, 2020.
@@ -25,25 +26,37 @@ function [] = ImshowTiff(image, index, enhanced)
         return;
     end
     
+    if nargin<4, zoomIn = 1;end
     if nargin<3, enhanced = false;end
     if nargin<2 || isempty(index), index = 1:size(image.image, 3);end
     N = length(index);
     ncol = ceil(sqrt(16/9*N));
     nrow = ceil(N / ncol);
+    % zoom in calculation
+    imageX = size(image.image, 1);
+    imageY = size(image.image, 2);
+    xzoomIn(1) = ceil(imageX / 2 * (1 - 1.0/zoomIn));
+    xzoomIn(2) = imageX - xzoomIn(1);
+    yzoomIn(1) = ceil(imageY / 2 * (1 - 1.0/zoomIn));
+    yzoomIn(2) = imageY - yzoomIn(1);
+    if xzoomIn(1)<1, xzoomIn(1)=1;end
+    if yzoomIn(1)<1, yzoomIn(1)=1;end
+    % enhance contrast
     if enhanced
         t = image.image(:, :, index);
         t = t(:);
         lowerBound = quantile(t, 0.003);
         upperBound = quantile(t, 0.997);
     end
+    % plot
     ha = tight_subplot(nrow, ncol);
     for i = 1:N
 
         axes(ha(i)); %#ok
         if (enhanced)
-            imshow(image.image(:, :, index(i)), [lowerBound, upperBound]);
+            imshow(image.image(xzoomIn(1):xzoomIn(2), yzoomIn(1):yzoomIn(2), index(i)), [lowerBound, upperBound]);
         else
-            imshow(image.image(:, :, index(i)));
+            imshow(image.image(xzoomIn(1):xzoomIn(2), yzoomIn(1):yzoomIn(2), index(i)));
         end
         colormap(pink);
         titleStr = "C:" + image.meta(1, index(i)) + " S:" + image.meta(2, index(i)) + " F:" + image.meta(3, index(i));
