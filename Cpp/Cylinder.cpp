e1d5248fb9465bdccee93c5eb3991dfd3ed5865e
Ziyi-WSL <ziyiz@DESKTOP-BFMTK5F.localdomain>
2020-05-25 13:25:42 -0400

    temporary

--- Cpp/Cylinder.cpp
+++ Cpp/Cylinder.cpp
@@ -1,7 +1,24 @@
 #include <Cylinder.h>
+#include <cmath>
 
 namespace zebrafish {
 
+bool cylinder::SampleCylinder(const Eigen::MatrixXd &image) {
 
+    const double &x = this->cyl.x;
+    const double &y = this->cyl.y;
+    const double &z = this->cyl.z;
+    const double &r = this->cyl.r;
+    const double &h = this->cyl.h;
+    const int &xmax = image.rows();
+    const int &ymax = image.cols();
+
+    // boundary check
+    if (x < -0.5 + r*1.5 || x > xmax-0.5-r*1.5 ||
+        y < -0.5 + r*1.5 || y > ymax-0.5-r*1.5 ||
+        z < 0 || z > ) {
+        return false;
+    }
+}
 
 }  // namespace zebrafish
--- Cpp/Cylinder.h
+++ Cpp/Cylinder.h
@@ -4,25 +4,45 @@
 #include <Eigen/Dense>
 ////////////////////////////////////////////////////////////////////////////////
 
+#define heightLayers 5  // desired height layers used in quadrature
+
+////////////////////////////////////////////////////////////////////////////////
+
 namespace zebrafish {
 
 typedef struct cylinder {
 // uniquely defines a cylinder
-    double x, y, z, r, h;  // bottom x, y, z + radius + height
+    double x, y, z, r, h;  // bottom x(row), y(col), z + radius + height
 } cylinder_t;
 
+
+typedef struct samplePoints {
+//
+    Eigen::MatrixX3d points;  // [#points x 3] point positions
+    Eigen::VectorXd weights;  // [#points] (Gaussian quadrature weight)*(energy function constants)*(subtraction sigma function)
+} samplePoints_t;
+
+///////////////////////////////////////
 
 class cylinder {
 
 private:
     cylinder_t cyl;
+    double energy;  // evaluated energy function value
+    samplePoints_t samplePoints;
 
 public:
-    void SampleCylinder();
-    /// Calculate the sample points (both interior and exterior) and save them to private variables
-    
-    void EvaluateCylinder();
-    /// Calculate 
+    bool SampleCylinder(const Eigen::MatrixXd &image);
+    /// Calculate the sample points (both interior and exterior point) and 
+    /// save them to corresponding private variables
+    /// 'image' is only used to do boundary check
+    ///
+    /// @param[in]  image     { 3D matrix of the image }
+    /// @return      {whether the cylinder is inside the boundary of the image}
+    ///
+
+    void EvaluateCylinder(const Eigen::MatrixXd &image);
+    /// Calculate the energy for this cylinder and save the value to a private variable "energy"
 
     // maintenance methods
     cylinder();
