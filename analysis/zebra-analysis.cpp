8e62cbf6aff3be5fd065e314ea8fe5897d8f18df
teseoch <teseo.schneider@gmail.com>
2025-07-09 15:26:58 -0700

    export done

--- analysis/zebra-analysis.cpp
+++ analysis/zebra-analysis.cpp
@@ -247,24 +247,24 @@ bool compute_analysis(
 
   const auto set_bc = [&barys, &box_min, &box_max](const Eigen::MatrixXd &v,
                                                    const bool boundary) {
-    double min = std::numeric_limits<double>::max();
-    int min_index = 0;
-    for (int i = 0; i < barys.rows(); ++i) {
-      const double norm = (v - barys.row(i)).squaredNorm();
-      if (norm < min) {
-        min_index = i;
-        min = norm;
-      }
-    }
+    // double min = std::numeric_limits<double>::max();
+    // int min_index = 0;
+    // for (int i = 0; i < barys.rows(); ++i) {
+    //   const double norm = (v - barys.row(i)).squaredNorm();
+    //   if (norm < min) {
+    //     min_index = i;
+    //     min = norm;
+    //   }
+    // }
 
-    if (min < 0.01)
-      return 2;
+    // if (min < 0.02)
+    //   return 2;
 
     if (boundary) {
       if (fabs(v(2) - box_min(2)) < 0.01)
         return 1;
       if (fabs(v(2) - box_max(2)) < 0.01)
-        return 1; // change me to 0 if free to move
+        return 3; // change me to 0 if free to move
 
       return 0;
     }
@@ -274,30 +274,51 @@ bool compute_analysis(
 
   /////////////////////////////////////////////////////////////////////////////////////////////
   // polyfem
-  //////////////////HERE/////////////////////////////////////////////////////////////////////
-  // polyfem::State state;
-  // state.mesh = std::make_unique<polyfem::Mesh3D>();
-  // state.mesh->build_from_matrices(nodes, elem);
-  // // igl::writeMESH("out.mesh", nodes, elem, faces);
-  // state.args["normalize_mesh"] = false;
-  // state.args["n_refs"] = n_refs;
-  // state.args["vismesh_rel_area"] = vismesh_rel_area;
-  // state.load_mesh();
-  // state.mesh->compute_boundary_ids(set_bc);
-  // END
-  // HERE/////////////////////////////////////////////////////////////////////
+  igl::writeMESH("analysis.mesh", nodes, elem, faces);
 
-  // {
-  //     Eigen::MatrixXd points;
-  //     Eigen::MatrixXi faces;
-  //     Eigen::MatrixXd sidesets;
+  std::vector<int> bottom, top, middle;
+  for (int i = 0; i < nodes.rows(); ++i) {
+    const auto tag = set_bc(nodes.row(i), true);
 
-  //     state.get_sidesets(points, faces, sidesets);
-  //     igl::write_triangle_mesh("text.obj", points, faces);
-  //     igl::write_triangle_mesh("text1.obj", V0, F);
-  // }
+    if (tag == 1) {
+      bottom.push_back(i);
+    } else if (tag == 3) {
+      top.push_back(i);
+    } else if (tag == 2) {
+      middle.push_back(i);
+    }
+  }
+
+  for (int i = 0; i < result_f_.rows(); ++i) {
+    for (int j = 0; j < 3; ++j) {
+      middle.push_back(result_f_(i, j));
+    }
+  }
+  std::sort(middle.begin(), middle.end());
+  middle.erase(std::unique(middle.begin(), middle.end()), middle.end());
 
   const Eigen::RowVector3d zero(0, 0, 0);
+
+  const std::string out_path = path + "-analysis.hdf5";
+  std::cout << "Exporting analysis to " << out_path << std::endl;
+
+  H5Easy::File file(out_path, H5Easy::File::ReadWrite | H5Easy::File::Create);
+  H5Easy::dump(file, "mesh/v", nodes);
+  H5Easy::dump(file, "mesh/t", elem);
+  H5Easy::dump(file, "mesh/f", faces);
+
+  H5Easy::dump(file, "bc/bottom", bottom);
+  H5Easy::dump(file, "bc/top", top);
+  H5Easy::dump(file, "bc/middle", middle);
+
+  H5Easy::dump(file, "problem/is_linear", is_linear);
+  H5Easy::dump(file, "problem/order", discr_order);
+  H5Easy::dump(file, "problem/E", E / double(1e6)); // E has unit [Pascal],
+  H5Easy::dump(file, "problem/nu", nu);
+
+  H5Easy::dump(file, "bc_func/eps", eps);
+  // H5Easy::dump(file, "bc_func/rbf_function", rbf_function);
+  H5Easy::dump(file, "bc_func/centers", bm_v);
 
   int fStart = frameStart;
   int fEnd = frameEnd;
@@ -306,182 +327,11 @@ bool compute_analysis(
   if (frameEnd == -1)
     fEnd = V.size() - 1;
   for (int sim = fStart; sim <= fEnd; ++sim) {
-    // HERE/////////////////////////////////////////////////////////////////////
-    //  const json args = {
-    //      {"tensor_formulation", is_linear ? "LinearElasticity" :
-    //      "NeoHookean"},
-    //      {"discr_order", discr_order},
-    //      {"params",
-    //       {{"E", E / double(1e6)},
-    //        {"nu", nu}}}, // E has unit [Pascal], convert that to [um] unit
-    //      {"problem", "PointBasedTensor"}};
+    const Eigen::MatrixXd currentv = V[sim].block(0, 0, bm_offset_v, 3) - bm_v;
 
-    // const Eigen::MatrixXd currentv = V[sim].block(0, 0, bm_offset_v, 3) -
-    // bm_v;
-
-    ///////HERE/////////////////////////////////////////////////////////////////////
-    // state.init(args);
-    // polyfem::PointBasedTensorProblem &tproblem =
-    //     *dynamic_cast<polyfem::PointBasedTensorProblem
-    //     *>(state.problem.get());
-    // tproblem.add_constant(1, zero);
-    // Eigen::Matrix<bool, 3, 1> dirichet_dims;
-    // dirichet_dims.setConstant(true);
-    // tproblem.add_function(2, currentv, bm_v, rbf_function, eps, -1,
-    //                       dirichet_dims);
-    // state.solve();
-
-    // // output
-    // const auto OutputHelper = [&state, &belowCage,
-    //                            &aboveCage](const std::string &out_path,
-    //                                        const Eigen::MatrixXd &mesh_v,
-    //                                        const MatrixXi &mesh_f) {
-    //   Eigen::MatrixXd displacement_vec;
-    //   Eigen::MatrixXd traction_forces, traction_forces_flip;
-    //   Eigen::MatrixXd stress, stress_flip;
-    //   Eigen::MatrixXd mises, mises_flip;
-    //   Eigen::MatrixXi mesh_f_flip = mesh_f;
-    //   mesh_f_flip.col(0) = mesh_f.col(1);
-    //   mesh_f_flip.col(1) = mesh_f.col(0);
-
-    //   state.interpolate_boundary_function_at_vertices(mesh_v, mesh_f,
-    //   state.sol,
-    //                                                   displacement_vec);
-    //   // guaranteed that (belowCage || aboveCage) == True
-    //   if (belowCage)
-    //     state.interpolate_boundary_tensor_function(
-    //         mesh_v, mesh_f, state.sol, displacement_vec, true,
-    //         traction_forces, stress, mises);
-    //   if (aboveCage)
-    //     state.interpolate_boundary_tensor_function(
-    //         mesh_v, mesh_f_flip, state.sol, displacement_vec, true,
-    //         traction_forces_flip, stress_flip, mises_flip);
-
-    //   // per-triangle data -> per-vertex data by taking average
-    //   const auto ToVertexData =
-    //       [&mesh_v, &mesh_f](const Eigen::MatrixXd &traction_forces_tri,
-    //                          const Eigen::MatrixXd &stress_tri,
-    //                          const Eigen::MatrixXd &mises_tri,
-    //                          Eigen::MatrixXd &traction_forces_ver,
-    //                          Eigen::MatrixXd &stress_ver,
-    //                          Eigen::MatrixXd &mises_ver) {
-    //         traction_forces_ver = Eigen::MatrixXd::Zero(mesh_v.rows(), 3);
-    //         stress_ver = Eigen::MatrixXd::Zero(mesh_v.rows(), 9); // 3x3
-    //         matrix mises_ver = Eigen::MatrixXd::Zero(mesh_v.rows(), 1);
-
-    //         Eigen::VectorXd area, vertex_area(mesh_v.rows());
-    //         vertex_area.setZero();
-    //         igl::doublearea(mesh_v, mesh_f, area);
-
-    //         for (int f = 0; f < mesh_f.rows(); ++f) {
-    //           for (int d = 0; d < 3; ++d) {
-    //             int vid = mesh_f(f, d);
-    //             traction_forces_ver.row(vid) +=
-    //                 traction_forces_tri.row(f) * area(f);
-    //             stress_ver.row(vid) += stress_tri.row(f) * area(f);
-    //             mises_ver(vid) += mises_tri(f) * area(f);
-    //             vertex_area(vid) += area(f);
-    //           }
-    //         }
-
-    //         for (int d = 0; d < 3; ++d) {
-    //           traction_forces_ver.col(d).array() /= vertex_area.array();
-    //         }
-    //         for (int d = 0; d < 9; ++d) {
-    //           stress_ver.col(d).array() /= vertex_area.array();
-    //         }
-    //         mises_ver.array() /= vertex_area.array();
-    //       };
-
-    //   // triangle to vertex
-    //   Eigen::MatrixXd traction_force_v, traction_force_v_flip,
-    //       traction_force_res;
-    //   Eigen::MatrixXd stress_v, stress_v_flip, stress_res;
-    //   Eigen::MatrixXd mises_v, mises_v_flip, mises_res;
-    //   if (belowCage)
-    //     ToVertexData(traction_forces, stress, mises, traction_force_v,
-    //     stress_v,
-    //                  mises_v);
-    //   if (aboveCage)
-    //     ToVertexData(traction_forces_flip, stress_flip, mises_flip,
-    //                  traction_force_v_flip, stress_v_flip, mises_v_flip);
-    //   std::string suffix = "";
-    //   if (aboveCage && !belowCage) {
-    //     traction_force_res = traction_force_v_flip;
-    //     stress_res = stress_v_flip;
-    //     mises_res = mises_v_flip;
-    //     suffix = "_above";
-    //   } else if (!aboveCage && belowCage) {
-    //     traction_force_res = traction_force_v;
-    //     stress_res = stress_v;
-    //     mises_res = mises_v;
-    //     suffix = "_below";
-    //   } else if (aboveCage && belowCage) {
-    //     traction_force_res =
-    //         (traction_force_v_flip + traction_force_v).array() / 2.0;
-    //     stress_res = (stress_v_flip + stress_v).array() / 2.0;
-    //     mises_res = (mises_v_flip + mises_v).array() / 2.0;
-    //     suffix = "_avg";
-    //   } else {
-    //     std::cerr << "neither aboveCage or belowCage? impossible!" <<
-    //     std::endl; return;
-    //   }
-
-    //   // write to VTU
-    //   polyfem::VTUWriter VTUwriter;
-    //   VTUwriter.add_field("displacement", displacement_vec);
-    //   VTUwriter.add_field("traction_forces" + suffix, traction_force_res);
-
-    //   for (int i = 0; i < 3; ++i) {
-    //     for (int j = 0; j < 3; ++j) {
-    //       const std::string index = "_" + std::to_string(i) +
-    //       std::to_string(j); VTUwriter.add_field("stress" + suffix + index,
-    //                           stress_res.col(i * 3 + j));
-    //     }
-    //   }
-
-    //   VTUwriter.add_field("von_mises" + suffix, mises_res);
-
-    //   VTUwriter.write_mesh(out_path, mesh_v, mesh_f);
-    // }; // OutputHelper lambda function
-    // END
-    ///////HERE/////////////////////////////////////////////////////////////////////
-
-    const auto RemoveOneRing = [&result_f](Eigen::MatrixXi &new_f) {
-      std::vector<int> boundary_v;
-      igl::boundary_loop(result_f, boundary_v);
-      new_f.resizeLike(result_f);
-      int cnt = 0;
-      for (int i = 0; i < result_f.rows(); i++) {
-        if (std::find(boundary_v.begin(), boundary_v.end(), result_f(i, 0)) !=
-                boundary_v.end() ||
-            std::find(boundary_v.begin(), boundary_v.end(), result_f(i, 1)) !=
-                boundary_v.end() ||
-            std::find(boundary_v.begin(), boundary_v.end(), result_f(i, 2)) !=
-                boundary_v.end()) {
-          // if this triangle is at boundary
-          continue;
-        } else {
-          new_f.row(cnt) = result_f.row(i);
-          cnt++;
-        }
-      }
-      new_f.conservativeResize(cnt, 3);
-    }; // KillOneRing lambda function
-
-    const std::string out_path = path + "-frame" + std::to_string(sim);
-    Eigen::MatrixXi result_f_without_boundary;
-    RemoveOneRing(result_f_without_boundary);
-    // HERE/////////////////////////////////////////////////////////////////////
-    //  OutputHelper(out_path + ".withBoundary.vtu", result_v, result_f);
-    //  OutputHelper(out_path + ".vtu", result_v, result_f_without_boundary);
-    //  state.save_vtu(out_path + ".all.vtu", 0);
-    //  state.save_surface(out_path + ".surf.vtu");
-    // end
-    //  HERE/////////////////////////////////////////////////////////////////////
-
-    std::cout << out_path << "  analysis and export done!" << std::endl;
+    H5Easy::dump(file, "bc_func/disp" + std::to_string(sim), currentv);
   }
+  std::cout << out_path << "  analysis and export done!" << std::endl;
 
   return true;
 }
