429edd8864776ca60e9540c9969b1be6bd2b51d6
teseoch <teseo.schneider@gmail.com>
2025-07-09 10:14:28 -0700

    move along z

--- Cpp/gui/GUI_markerDrag.cpp
+++ Cpp/gui/GUI_markerDrag.cpp
@@ -3,171 +3,204 @@
 
 #include <igl/unproject_onto_mesh.h>
 
-
 namespace zebrafish {
 
-namespace {
-
-}  // anonymous namespace
+namespace {} // anonymous namespace
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // Special module about mouse draging a marker
 
 void GUI::RenderMarkerDragGUI() {
 
-    if (ImGui::TreeNode("Manually move markers")) {
+  if (ImGui::TreeNode("Manually move markers")) {
 
-        const float inputWidth = ImGui::GetWindowWidth() / 3.0;
-        ImGui::PushItemWidth(inputWidth);
-        ImGui::Checkbox("[Mouse] Move markers", &markerDragActive);
-        if (showTooltip && ImGui::IsItemHovered()) {
-            ImGui::SetTooltip("Manually change the XY location of the markers using mouse.\nCheck this box and move close to a marker. Click and hold to select one marker. Drag it to a new location and release button.");
-        }
-        if (ImGui::Button("Optimize this frame")) {
-            MarkerDragReset();
-            MarkerDepthCorrection(frameToShow, 0, 0, false);
-            // update visualization variable
-            UpdateMarkerPointLocArray();
-        }
-        if (showTooltip && ImGui::IsItemHovered()) {
-            ImGui::SetTooltip("Optimize the XY location of markers in this frame again. This is optional.\nIf any marker has been manually moved, it is highly suggested to optimize this frame again.\nThis button does not optimize the depth.");
-        }
-        ImGui::PopItemWidth();
-
-        ImGui::TreePop();
-        ImGui::Separator();
+    const float inputWidth = ImGui::GetWindowWidth() / 3.0;
+    ImGui::PushItemWidth(inputWidth);
+    ImGui::Checkbox("[Mouse] Move markers", &markerDragActive);
+    if (showTooltip && ImGui::IsItemHovered()) {
+      ImGui::SetTooltip(
+          "Manually change the XY location of the markers using mouse.\nCheck "
+          "this box and move close to a marker. Click and hold to select one "
+          "marker. Drag it to a new location and release button.");
     }
-}
+    if (ImGui::Button("Optimize this frame")) {
+      MarkerDragReset();
+      MarkerDepthCorrection(frameToShow, 0, 0, false);
+      // update visualization variable
+      UpdateMarkerPointLocArray();
+    }
+    if (showTooltip && ImGui::IsItemHovered()) {
+      ImGui::SetTooltip(
+          "Optimize the XY location of markers in this frame again. This is "
+          "optional.\nIf any marker has been manually moved, it is highly "
+          "suggested to optimize this frame again.\nThis button does not "
+          "optimize the depth.");
+    }
 
+    static int currentSelectedMarker = 0;
+    static double deltaZ = 0.0;
+    const Eigen::MatrixXd &loc = markerPointLocArray[frameToShow];
+    const int N = loc.rows();
+
+    ImGui::Separator();
+    ImGui::Text("Move marker on z");
+    ImGui::SliderInt("Marker", &currentSelectedMarker, 0, N - 1);
+
+    ImGui::InputDouble("Amount", &deltaZ, 0.0, 0.0, "%.2f");
+    if (ImGui::Button("Move z")) {
+      // override the location
+      markerRecord_t &markerRecord = markerArray[frameToShow];
+      if (currentSelectedMarker < 0 ||
+          currentSelectedMarker > markerRecord.num - 1) {
+        logger().error("[Fatal error] currentSelectedMarker is invalid: {}",
+                       currentSelectedMarker);
+      } else {
+        markerRecord.loc(currentSelectedMarker, 2) += deltaZ;
+        UpdateMarkerPointLocArray();
+        logger().debug("Manually move marker: index {} moved by {}",
+                       currentSelectedMarker, deltaZ);
+      }
+    }
+
+    ImGui::PopItemWidth();
+
+    ImGui::TreePop();
+    ImGui::Separator();
+  }
+}
 
 void GUI::MouseSelectMarker(const Eigen::Vector2f &mouse) {
-/// called by "MouseMoveCallback" when "markerDragActive" is true and "markerDragFocused" is false
+  /// called by "MouseMoveCallback" when "markerDragActive" is true and
+  /// "markerDragFocused" is false
 
-    static int fid;
-    static Eigen::Vector3f bc;
-    static double x, y;
-    static bool hit;
+  static int fid;
+  static Eigen::Vector3f bc;
+  static double x, y;
+  static bool hit;
 
-    hit = igl::unproject_onto_mesh(
-        Eigen::Vector2f(mouse(0), viewer.core().viewport(3)-mouse(1)), 
-        viewer.core().view, 
-        viewer.core().proj,
-        viewer.core().viewport, 
-        V_texture, 
-        F_texture, 
-        fid, 
-        bc);
+  hit = igl::unproject_onto_mesh(
+      Eigen::Vector2f(mouse(0), viewer.core().viewport(3) - mouse(1)),
+      viewer.core().view, viewer.core().proj, viewer.core().viewport, V_texture,
+      F_texture, fid, bc);
 
-    if (hit) {
-        // has hit
+  if (hit) {
+    // has hit
 
-        y =                 (V_texture(F_texture(fid, 0), 0) * bc(0) + V_texture(F_texture(fid, 1), 0) * bc(1) + V_texture(F_texture(fid, 2), 0) * bc(2)) - 0.5;
-        x = imgRows - 0.5 - (V_texture(F_texture(fid, 0), 1) * bc(0) + V_texture(F_texture(fid, 1), 1) * bc(1) + V_texture(F_texture(fid, 2), 1) * bc(2));
+    y = (V_texture(F_texture(fid, 0), 0) * bc(0) +
+         V_texture(F_texture(fid, 1), 0) * bc(1) +
+         V_texture(F_texture(fid, 2), 0) * bc(2)) -
+        0.5;
+    x = imgRows - 0.5 -
+        (V_texture(F_texture(fid, 0), 1) * bc(0) +
+         V_texture(F_texture(fid, 1), 1) * bc(1) +
+         V_texture(F_texture(fid, 2), 1) * bc(2));
 
-        markerDragLoc << x, y;
+    markerDragLoc << x, y;
 
-        if (!markerDragFocused) {
-            // search for the nearest marker
-            double dist_square, minDistSquare;
-            const double distSquareThres = 3.0 * 3.0;
-            minDistSquare = distSquareThres;  // reset
-            markerRecord_t &markerRecord = markerArray[frameToShow];
-            for (int i=0; i<markerRecord.num; i++) {
+    if (!markerDragFocused) {
+      // search for the nearest marker
+      double dist_square, minDistSquare;
+      const double distSquareThres = 3.0 * 3.0;
+      minDistSquare = distSquareThres; // reset
+      markerRecord_t &markerRecord = markerArray[frameToShow];
+      for (int i = 0; i < markerRecord.num; i++) {
 
-                dist_square = (markerRecord.loc(i, 0)-x)*(markerRecord.loc(i, 0)-x) + (markerRecord.loc(i, 1)-y)*(markerRecord.loc(i, 1)-y);
-                if (dist_square >= mousePickDistSquareThres) continue;
-                if (dist_square < minDistSquare) {
-                    // find a new nearest marker
+        dist_square =
+            (markerRecord.loc(i, 0) - x) * (markerRecord.loc(i, 0) - x) +
+            (markerRecord.loc(i, 1) - y) * (markerRecord.loc(i, 1) - y);
+        if (dist_square >= mousePickDistSquareThres)
+          continue;
+        if (dist_square < minDistSquare) {
+          // find a new nearest marker
 
-                    minDistSquare = dist_square;
-                    markerDragHitIndex = i;
-                }
-            }
-            // update markerDragHit
-            markerDragHit = minDistSquare < distSquareThres;
+          minDistSquare = dist_square;
+          markerDragHitIndex = i;
         }
-    } else {
-
-        // update markerDragHit
-        markerDragHit = false;
+      }
+      // update markerDragHit
+      markerDragHit = minDistSquare < distSquareThres;
     }
-}
+  } else {
 
+    // update markerDragHit
+    markerDragHit = false;
+  }
+}
 
 void GUI::MarkerDragSelect() {
 
-    markerDragFocused = true;
-    logger().debug("Manual move marker: select index {}", markerDragHitIndex);
+  markerDragFocused = true;
+  logger().debug("Manual move marker: select index {}", markerDragHitIndex);
 }
-
 
 void GUI::MarkerDragSetNewLoc() {
 
-    double x = markerDragLoc(0);
-    double y = markerDragLoc(1);
+  double x = markerDragLoc(0);
+  double y = markerDragLoc(1);
 
-    // override the location
-    markerRecord_t &markerRecord = markerArray[frameToShow];
-    if (markerDragHitIndex < 0 || markerDragHitIndex > markerRecord.num-1) {
-        logger().error("[Fatal error] markerDragHitIndex is invalid: {}", markerDragHitIndex);
-    } else {
-        markerRecord.loc(markerDragHitIndex, 0) = x;
-        markerRecord.loc(markerDragHitIndex, 1) = y;
-        logger().debug("Manually move marker: index {} moved to location {}x{}", markerDragHitIndex, x, y);
-    }
+  // override the location
+  markerRecord_t &markerRecord = markerArray[frameToShow];
+  if (markerDragHitIndex < 0 || markerDragHitIndex > markerRecord.num - 1) {
+    logger().error("[Fatal error] markerDragHitIndex is invalid: {}",
+                   markerDragHitIndex);
+  } else {
+    markerRecord.loc(markerDragHitIndex, 0) = x;
+    markerRecord.loc(markerDragHitIndex, 1) = y;
+    logger().debug("Manually move marker: index {} moved to location {}x{}",
+                   markerDragHitIndex, x, y);
+  }
 
-    // update markerDrag flags
-    markerDragHit = false;
-    markerDragFocused = false;
-    markerDragHitIndex = -1;
+  // update markerDrag flags
+  markerDragHit = false;
+  markerDragFocused = false;
+  markerDragHitIndex = -1;
 
-    // update visualization
-    UpdateMarkerPointLocArray();
+  // update visualization
+  UpdateMarkerPointLocArray();
 }
-
 
 void GUI::MarkerDragReset() {
 
-    markerDragActive = false;
-    markerDragHit = false;
-    markerDragFocused = false;
-    markerDragHitIndex = -1;
+  markerDragActive = false;
+  markerDragHit = false;
+  markerDragFocused = false;
+  markerDragHitIndex = -1;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // Visualization
 
-
 void GUI::MarkerDragVisualization() {
 
-    if (!markerDragActive) return;
+  if (!markerDragActive)
+    return;
 
-    // change color to the chosen marker
-    if (markerDragHit) {
+  // change color to the chosen marker
+  if (markerDragHit) {
 
-        Eigen::MatrixXd selectedLoc(1, 3);
-        static const double deltaZ = 0.3;
-        selectedLoc(0) = markerPointLocArray[frameToShow](markerDragHitIndex, 0);
-        selectedLoc(1) = markerPointLocArray[frameToShow](markerDragHitIndex, 1);
-        selectedLoc(2) = markerPointLocArray[frameToShow](markerDragHitIndex, 2) + deltaZ;
+    Eigen::MatrixXd selectedLoc(1, 3);
+    static const double deltaZ = 0.3;
+    selectedLoc(0) = markerPointLocArray[frameToShow](markerDragHitIndex, 0);
+    selectedLoc(1) = markerPointLocArray[frameToShow](markerDragHitIndex, 1);
+    selectedLoc(2) =
+        markerPointLocArray[frameToShow](markerDragHitIndex, 2) + deltaZ;
 
-        Eigen::MatrixXd pointColor(1, 3);
-        pointColor << 0.0, 1.0, 1.0;
-        viewer.data().add_points(selectedLoc, pointColor);
-    }
+    Eigen::MatrixXd pointColor(1, 3);
+    pointColor << 0.0, 1.0, 1.0;
+    viewer.data().add_points(selectedLoc, pointColor);
+  }
 
-    // add a new visualization point at tip of mouse
-    if (markerDragFocused) {
+  // add a new visualization point at tip of mouse
+  if (markerDragFocused) {
 
-        Eigen::MatrixXd destLoc(1, 3);
-        static const double deltaZ = 0.3;
-        destLoc(0) = markerDragLoc(1) + 0.5;
-        destLoc(1) = (imgRows-0.5) - markerDragLoc(0);
-        destLoc(2) = markerPointLocArray[frameToShow](markerDragHitIndex, 2);
+    Eigen::MatrixXd destLoc(1, 3);
+    static const double deltaZ = 0.3;
+    destLoc(0) = markerDragLoc(1) + 0.5;
+    destLoc(1) = (imgRows - 0.5) - markerDragLoc(0);
+    destLoc(2) = markerPointLocArray[frameToShow](markerDragHitIndex, 2);
 
-        viewer.data().add_points(destLoc, markerPointColor);
-    }
+    viewer.data().add_points(destLoc, markerPointColor);
+  }
 }
 
-}  // namespace zebrafish
+} // namespace zebrafish
